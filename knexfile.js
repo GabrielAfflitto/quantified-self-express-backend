5e857bc210dd704800d7005983c15f1f3f5de0f1
TMad <madsen.tyler@gmail.com>
2018-03-23 17:06:01 -0600

    Update knex production database name

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgresql-adjacent-85197',
+    connection: 'postgres://tamnfdjdownlms:3ac7ea73db7ed248aaba3ef4b688b6025d71d81d37a29f3a3f6f29174fbd7c97@ec2-54-243-210-70.compute-1.amazonaws.com:5432/db0q3cbdr3t9sh',
     migrations: {
       directory: './db/migrations'
     },
