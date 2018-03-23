6fcb1c88f295a8603a27f9b91eafaed84f9a341a
TMad <madsen.tyler@gmail.com>
2018-03-23 17:02:29 -0600

    Add knex production database name

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: 'quantified-self-api',
     migrations: {
       directory: './db/migrations'
     },
