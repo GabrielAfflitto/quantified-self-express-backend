33ff4a4f25287c3746c8d7a9331220ad69c253cd
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-26 15:12:14 -0600

    created show function in meals controller

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,4 +1,5 @@
 const allMeals = require('../models/mealsModel').allMeals;
+const showMeal = require('../models/mealsModel').showMeal;
 
 const index = function(req, res, next) {
   allMeals().then(function(mealFoods) {
@@ -23,31 +24,53 @@ const index = function(req, res, next) {
       res.json(output)
     }
   })
-
-  function createMealObject(mealFood) {
-    let object = {}
-    object['id'] = mealFood.meal_id
-    object['name'] = mealFood.meal_name
-    object['foods'] = []
-    return object
-  }
-
-  function createFoodObject(mealFood) {
-    let object = {}
-    object['id'] = mealFood.food_id
-    object['name'] = mealFood.food_name
-    object['calories'] = mealFood.calories
-    return object
-  }
-
-  function containsObject(object, output) {
-    for (let i = 0; i < output.length; i++) {
-      if (output[i].id === object.id) {
-        return true;
-      }
-    }
-    return false;
-  }
 }
 
-module.exports = {index}
+const show = function(req, res, next) {
+  let id = req.params.id
+  showMeal(id).then(function(mealFoods){
+    if(!mealFoods.rows){
+      return res.sendStatus(404)
+    } else {
+      let output;
+      mealFoods.rows.forEach(function(mealFood, index) {
+        let foodObject = createFoodObject(mealFood)
+        if (index === 0) {
+          let mealObject = createMealObject(mealFood)
+          mealObject.foods.push(foodObject)
+          output = mealObject
+        } else {
+          output.foods.push(foodObject)
+        }
+      })
+      res.json(output)
+    }
+  })
+}
+
+const createMealObject = function(mealFood) {
+  let object = {}
+  object['id'] = mealFood.meal_id
+  object['name'] = mealFood.meal_name
+  object['foods'] = []
+  return object
+}
+
+const createFoodObject = function(mealFood) {
+  let object = {}
+  object['id'] = mealFood.food_id
+  object['name'] = mealFood.food_name
+  object['calories'] = mealFood.calories
+  return object
+}
+
+const containsObject = function(object, output) {
+  for (let i = 0; i < output.length; i++) {
+    if (output[i].id === object.id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = { index, show}
