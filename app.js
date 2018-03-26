4becfd2f83214d6b640edfeebdf7c04993ff8fc8
TMad <madsen.tyler@gmail.com>
2018-03-26 17:00:38 -0600

    Change allow access control header methods

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://gabrielafflitto.github.io");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 app.use(logger('dev'));
