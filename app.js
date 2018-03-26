b4e552d85e4b2834e77e4859e4e9ba56d7f73b36
TMad <madsen.tyler@gmail.com>
2018-03-26 16:59:19 -0600

    Add access control allow methods

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://gabrielafflitto.github.io");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 app.use(logger('dev'));
