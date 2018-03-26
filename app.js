05a7ed78619a22e59f94caea2e3ed580bae23146
TMad <madsen.tyler@gmail.com>
2018-03-26 16:45:13 -0600

    Add CORS header

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://gabrielafflitto.github.io/quantified-self/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
