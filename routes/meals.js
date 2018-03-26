64c2fda3543d481e9436bef201d46508274e0627
TMad <madsen.tyler@gmail.com>
2018-03-26 16:14:37 -0600

    Add delete route to meal routes file

--- routes/meals.js
+++ routes/meals.js
@@ -1,6 +1,7 @@
 const index = require('../controllers/mealsController').index;
 const show = require('../controllers/mealsController').show;
 const create = require('../controllers/mealsController').create;
+const destroy = require('../controllers/mealsController').destroy;
 
 var express = require('express');
 var router = express.Router();
@@ -8,5 +9,6 @@ var router = express.Router();
 router.get('/', index);
 router.get('/:id/foods', show);
 router.post('/:meal_id/foods/:food_id', create);
+router.delete('/:meal_id/foods/:food_id', destroy);
 
 module.exports = router;
