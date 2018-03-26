ece393870636f35c1597a960c1e76ba791a6e329
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-26 11:43:46 -0600

    imported delete food function into food router

--- routes/foods.js
+++ routes/foods.js
@@ -6,6 +6,7 @@ const index = require('../controllers/foodsController').index;
 const show = require('../controllers/foodsController').show;
 const create = require('../controllers/foodsController').create;
 const update = require('../controllers/foodsController').update;
+const destroy = require('../controllers/foodsController').destroy;
 
 var express = require('express');
 var router = express.Router();
@@ -14,20 +15,6 @@ router.get('/', index)
 router.get('/:id', show)
 router.post('/', create)
 router.patch('/:id', update)
-
-router.delete('/:id', function(req, res, next) {
-  let id = req.params.id
-
-  database.raw(
-    'DELETE FROM foods WHERE id = ?', [id]
-  ).then(function(foods) {
-    if(!foods.rows) {
-      return res.sendStatus(404)
-    } else {
-      res.json(foods.rows)
-    }
-  })
-
-})
+router.delete('/:id', destroy)
 
 module.exports = router;
