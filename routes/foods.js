9a24918412504b3f5c0bbbc79283530f87df69a3
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-26 11:07:53 -0600

    import abstracted functions from controller into foods.js

--- routes/foods.js
+++ routes/foods.js
@@ -2,34 +2,14 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+const index = require('../controllers/foodsController').index;
+const show = require('../controllers/foodsController').show;
+
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  database.raw(
-    'SELECT * FROM foods'
-  ).then(function(foods) {
-    if(!foods.rows) {
-      return res.sendStatus(404)
-    } else {
-      res.json(foods.rows)
-    }
-  })
-});
-
-router.get('/:id', function(req, res, next) {
-  let id = req.params.id
-
-  database.raw(
-    'SELECT * FROM foods WHERE id = ?', [id]
-  ).then(function(foods) {
-    if(!foods.rows) {
-      return res.sendStatus(404)
-    } else {
-      res.json(foods.rows)
-    }
-  })
-});
+router.get('/', index)
+router.get('/:id', show)
 
 router.post('/', function(req, res, next) {
   var name = req.body.name
