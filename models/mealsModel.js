4ff123ceb9592896e8d94b9430afd24ccbc21005
TMad <madsen.tyler@gmail.com>
2018-03-26 17:22:45 -0600

    Capitalize SQL query

--- models/mealsModel.js
+++ models/mealsModel.js
@@ -2,7 +2,6 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
-
 const allMeals = function() {
   return database.raw(
       'SELECT m.id AS meal_id, m.name AS meal_name, f.id AS food_id, f.name AS food_name, f.calories AS calories FROM meals m JOIN meal_foods mf ON m.id = mf.meal_id JOIN foods f ON f.id = mf.food_id ORDER BY m.id, f.id'
@@ -24,7 +23,7 @@ const createMealFood = function(meal_id, food_id) {
 }
 
 const deleteMealFood = function(meal_id, food_id) {
-  return database.raw('DELETE FROM meal_foods WHERE id in (select id from meal_foods where meal_id = ? and food_id = ? LIMIT 1)', [meal_id, food_id])
+  return database.raw('DELETE FROM meal_foods WHERE id IN (SELECT id FROM meal_foods WHERE meal_id = ? AND food_id = ? LIMIT 1)', [meal_id, food_id])
 }
 
 
