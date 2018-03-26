587a0b9acd83bbc68850cf0ef47d3aa3adf37ab2
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-26 11:29:44 -0600

    imorted update foods function into foods router

--- routes/foods.js
+++ routes/foods.js
@@ -5,6 +5,7 @@ const database = require('knex')(configuration)
 const index = require('../controllers/foodsController').index;
 const show = require('../controllers/foodsController').show;
 const create = require('../controllers/foodsController').create;
+const update = require('../controllers/foodsController').update;
 
 var express = require('express');
 var router = express.Router();
@@ -12,37 +13,7 @@ var router = express.Router();
 router.get('/', index)
 router.get('/:id', show)
 router.post('/', create)
-
-router.patch('/:id', function(req, res, next) {
-
-  let id = req.params.id
-  if(req.body.name) {
-    var name = req.body.name
-  } else if(req.body.calories) {
-    var calories = req.body.calories
-  }
-
-  if(!name && !calories) {
-    return res.status(422).send({
-      error: "Name or calories are required to update a food"
-    })
-  }
-  if(name) {
-    database.raw(
-      'UPDATE foods SET name = ? WHERE id = ?',
-      [name, id]
-    ).then(function(food) {
-      res.status(201).json(food.rows)
-    })
-  } else if(calories) {
-    database.raw(
-      'UPDATE foods SET calories = ? WHERE id = ?',
-      [calories, id]
-    ).then(function(food) {
-      res.status(201).json(food.rows)
-    })
-  }
-});
+router.patch('/:id', update)
 
 router.delete('/:id', function(req, res, next) {
   let id = req.params.id
