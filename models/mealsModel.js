87d92e700c98d91febe3a361bd91b0b8373ea42d
TMad <madsen.tyler@gmail.com>
2018-03-28 14:47:53 -0600

    Fix indentation

--- models/mealsModel.js
+++ models/mealsModel.js
@@ -4,22 +4,22 @@ const database = require('knex')(configuration)
 
 const allMeals = function() {
   return database.raw(
-      'SELECT m.id AS meal_id, m.name AS meal_name, f.id AS food_id, f.name AS food_name, f.calories AS calories FROM meals m JOIN meal_foods mf ON m.id = mf.meal_id JOIN foods f ON f.id = mf.food_id ORDER BY m.id, f.id'
-    )
+    'SELECT m.id AS meal_id, m.name AS meal_name, f.id AS food_id, f.name AS food_name, f.calories AS calories FROM meals m JOIN meal_foods mf ON m.id = mf.meal_id JOIN foods f ON f.id = mf.food_id ORDER BY m.id, f.id'
+  )
 }
 
 const showMeal = function(id) {
   return database.raw(
-      'SELECT m.id AS meal_id, m.name AS meal_name, f.id AS food_id, f.name AS food_name, f.calories AS calories FROM meals m JOIN meal_foods mf ON m.id = mf.meal_id JOIN foods f ON f.id = mf.food_id WHERE m.id = ? ORDER BY m.id, f.id',
-      [id]
-    )
+    'SELECT m.id AS meal_id, m.name AS meal_name, f.id AS food_id, f.name AS food_name, f.calories AS calories FROM meals m JOIN meal_foods mf ON m.id = mf.meal_id JOIN foods f ON f.id = mf.food_id WHERE m.id = ? ORDER BY m.id, f.id',
+    [id]
+  )
 }
 
 const createMealFood = function(meal_id, food_id) {
   return database.raw(
-      'INSERT INTO meal_foods (meal_id, food_id) VALUES (?, ?)',
-      [meal_id, food_id]
-    )
+    'INSERT INTO meal_foods (meal_id, food_id) VALUES (?, ?)',
+    [meal_id, food_id]
+  )
 }
 
 const deleteMealFood = function(meal_id, food_id) {
